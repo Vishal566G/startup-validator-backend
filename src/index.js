384dec55df37692ef796a9ce46e61c9bfbdd8a99
Vishal <visku199@gmail.com>
2026-04-07 19:09:49 +0530

    add frontend URL in index.js

--- src/index.js
+++ src/index.js
@@ -9,7 +9,10 @@ const app = express();
 
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://your-app.vercel.app"],
+    origin: [
+      "http://localhost:5173",
+      "https://frontend-startup-validator.vercel.app/",
+    ],
   }),
 );
 app.use(express.json());
